95ec4559cdd8bae7c834c3de5f613d19f73272e1
luksvrd <ljvy9b@gmail.com>
2023-01-31 18:49:50 -0600

    fix body values

--- public/js/newBlog.js
+++ public/js/newBlog.js
@@ -2,11 +2,11 @@ const newPostHandler = async function (event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const body = document.querySelector('textarea[name="post-body"]');
+  const body = document.querySelector('textarea[name="post-body"]').value;
   // console.log(title);
   // console.log(body);
 
-  await fetch(`/api/posts`, {
+  await fetch(`/api/post`, {
     method: "POST",
     body: JSON.stringify({ title, body }),
     headers: { "Content-Type": "application/json" },
